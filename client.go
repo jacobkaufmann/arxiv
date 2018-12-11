8c9813bf66c2116e028142a764ecca1066ed650a
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2018-12-11 15:51:40 -0600

    Initial commit

---
+++ client.go
@@ -0,0 +1,141 @@
+package arxiv
+
+import (
+	"bytes"
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"reflect"
+
+	"github.com/google/go-querystring/query"
+)
+
+// A Client communicates with the arXiv API
+type Client struct {
+	Eprints    EprintsService
+	BaseURL    *url.URL
+	httpClient *http.Client
+}
+
+// NewClient creates a new HTTP Client for arXiv.  If httpClient == nil,
+// then http.DefaultClient is used.
+func NewClient(httpClient *http.Client) *Client {
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
+
+	c := &Client{
+		BaseURL: &url.URL{
+			Scheme: "http",
+			Host:   "export.arxiv.org",
+			Path:   "/api/",
+		},
+		httpClient: httpClient,
+	}
+	c.Eprints = &eprintsService{c}
+
+	return c
+}
+
+// QueryOptions specifies general pagination options for fetching a list of
+// results.
+type QueryOptions struct {
+	MaxResults int      `url:"max_results,omitempty"`
+	Start      int      `url:"start,omitempty"`
+	SortBy     string   `url:"sortBy,omitempty"`
+	SortOrder  string   `url:"sortOrder,omitempty"`
+	Search     string   `url:"search_query,omitempty"`
+	IDList     []string `url:"id_list,omitempty,comma"`
+}
+
+// DefaultMaxResults is the default number of items to return for a query
+const DefaultMaxResults = 100
+
+// url generates the URL to the named thesrc API endpoint, using the
+// specified route variables and query options.
+func (c *Client) url(apiRouteName string, opt interface{}) (*url.URL, error) {
+	url := &url.URL{
+		Path: apiRouteName,
+	}
+
+	if opt != nil {
+		err := addOptions(url, opt)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return url, nil
+}
+
+// NewRequest creates an API request. A relative URL can be provided in urlStr,
+// in which case it is resolved relative to the BaseURL of the Client. Relative
+// URLs should always be specified without a preceding slash. If specified, the
+// value pointed to by body is JSON encoded and included as the request body.
+func (c *Client) NewRequest(method, urlStr string, body interface{}) (*http.Request, error) {
+	rel, err := url.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+
+	u := c.BaseURL.ResolveReference(rel)
+
+	buf := new(bytes.Buffer)
+	if body != nil {
+		err := json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
+}
+
+// Do sends an API request and returns the API response. The API response is
+// XML-decoded and stored in the value pointed to by v, or returned as an error
+// if an API error has occurred.
+func (c *Client) Do(req *http.Request, v interface{}) (*http.Response, error) {
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if v != nil {
+		if bp, ok := v.(*[]byte); ok {
+			*bp, err = ioutil.ReadAll(resp.Body)
+		} else {
+			err = xml.NewDecoder(resp.Body).Decode(v)
+		}
+	}
+	if err != nil {
+		return nil, fmt.Errorf("error reading response from %s %s: %s", req.Method, req.URL.RequestURI(), err)
+	}
+	return resp, nil
+}
+
+// addOptions adds the parameters in opt as URL query parameters to u. opt
+// must be a struct whose fields may contain "url" tags.
+func addOptions(u *url.URL, opt interface{}) error {
+	v := reflect.ValueOf(opt)
+	if v.Kind() == reflect.Ptr && v.IsNil() {
+		return nil
+	}
+
+	qs, err := query.Values(opt)
+	if err != nil {
+		return err
+	}
+
+	u.RawQuery = qs.Encode()
+	return nil
+}
---
+++ eprints.go
@@ -0,0 +1,156 @@
+package arxiv
+
+import (
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"strings"
+
+	"golang.org/x/tools/blog/atom"
+)
+
+// An EprintFeed is a feed retrieved from the arXiv API
+type EprintsFeed struct {
+	XMLName xml.Name    `xml:"http://www.w3.org/2005/Atom feed"`
+	Title   string      `xml:"title"`
+	ID      string      `xml:"id"`
+	Updated string      `xml:"updated"`
+	Link    []atom.Link `xml:"link"`
+	Entry   []*Eprint   `xml:"entry"`
+}
+
+// A Category is a subject category for an e-print
+type Category struct {
+	Term string `xml:"term,attr"`
+}
+
+// An Author is an author of an e-print
+type Author struct {
+	Name string `xml:"name"`
+}
+
+// A Link is a link attached to an e-print
+type Link struct {
+	Rel   string `xml:"rel,attr,omitempty"`
+	Href  string `xml:"href,attr"`
+	Type  string `xml:"type,attr,omitempty"`
+	Title string `xml:"title,attr,omitempty"`
+}
+
+// An Eprint is an e-print residing in arXiv.org
+type Eprint struct {
+	ID         string     `xml:"id"`
+	Title      string     `xml:"title"`
+	Authors    []Author   `xml:"author"`
+	Links      []Link     `xml:"link"`
+	Categories []Category `xml:"category"`
+	Published  string     `xml:"published"`
+	Updated    string     `xml:"updated"`
+	Abstract   string     `xml:"summary"`
+}
+
+// EprintsService interacts with the e-print-related endpoints on arXiv's API
+type EprintsService interface {
+	Get(id string) (*Eprint, error)
+	List(opt *QueryOptions) ([]*Eprint, error)
+}
+
+var (
+	// ErrEprintNotFound is a failure to find a specified e-print
+	ErrEprintNotFound = errors.New("e-print not found")
+)
+
+// A SearchOptions is a search query configuration
+type SearchOptions struct {
+	Title            string
+	Author           string
+	Abstract         string
+	JournalReference string
+	Category         string
+	All              string
+}
+
+func (opt SearchOptions) String() string {
+	s := ""
+
+	if opt.All != "" {
+		s = fmt.Sprintf("all:%s", opt.All)
+	} else {
+		m := make(map[string]string)
+		m["ti"] = opt.Title
+		m["au"] = opt.Author
+		m["abs"] = opt.Abstract
+		m["jr"] = opt.JournalReference
+		m["cat"] = opt.Category
+
+		q := []string{}
+		for k, v := range m {
+			if v != "" {
+				q = append(q, fmt.Sprintf("%s:%s", k, v))
+			}
+		}
+
+		s = strings.Join(q, " AND ")
+	}
+
+	return s
+}
+
+type eprintsService struct{ client *Client }
+
+func (s *eprintsService) Get(id string) (*Eprint, error) {
+	opt := &QueryOptions{}
+	opt.IDList = append(opt.IDList, id)
+
+	url, err := s.client.url("query", opt)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := s.client.NewRequest("GET", url.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	feed := EprintsFeed{}
+	_, err = s.client.Do(req, &feed)
+	if err != nil {
+		return nil, err
+	}
+
+	eprints := feed.Entry
+	if len(eprints) == 0 {
+		return nil, ErrEprintNotFound
+	}
+
+	eprint := feed.Entry[0]
+
+	return eprint, nil
+}
+
+func (s *eprintsService) List(opt *QueryOptions) ([]*Eprint, error) {
+	url, err := s.client.url("query", opt)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("URL: %s\n", url.String())
+
+	req, err := s.client.NewRequest("GET", url.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	feed := EprintsFeed{}
+	_, err = s.client.Do(req, &feed)
+	if err != nil {
+		return nil, err
+	}
+
+	eprints := feed.Entry
+	if len(eprints) == 0 {
+		return nil, ErrEprintNotFound
+	}
+
+	return eprints, nil
+}
